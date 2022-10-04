6bf52af0f63951846a1f2fb14cf4eeb2d47ee699
SameepAher <sameepaher@gmail.com>
2022-10-04 16:14:30 +0530

    .env file added

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+const pass = process.env.PASSWORD;
+const name = process.env.NAME;
 
 mongoose
   .connect(
-    "mongodb+srv://sam:2HPpnBE6fObkLYKM@purplepebble.ems3e.mongodb.net/PurplePebble?retryWrites=true&w=majority"
+    `mongodb+srv://sam:${pass}@purplepebble.ems3e.mongodb.net/${name}?retryWrites=true&w=majority`
   )
   .then(() => {
     console.log(`Database Connection Successful`);
